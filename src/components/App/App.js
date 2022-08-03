ccf90243439b2bf29ecc571f8122617bc4fc0334
Rachel Bock <rachel.online@gmail.com>
2022-08-03 13:31:22 -0600

    Add component files

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bird Adventures</h1>
+        
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Checklist/Checklist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Checklist = () = {
+
+  
+  return (
+    <p>Checklist component</p>
+  )
+}
+
+export default Checklist;
